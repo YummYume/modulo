60353d40084a730cbbd41d205342e435e0b0f046
Tristan-Evenot <evenot.tristan@gmail.com>
2022-05-03 15:19:37 +0200

    404 page style

--- app/pages/404.js
+++ app/pages/404.js
@@ -1,3 +1,31 @@
+import Typography from "@mui/material/Typography";
+import Slide from "@mui/material/Slide";
+import Button from "@mui/material/Button";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import Link from "next/link";
+
 export default function Custom404() {
-    return <h1>404 - Page Not Found</h1>;
+    return (
+        <div className="d-flex flex-column justify-content-center align-items-center w-100">
+            <Typography variant="h1" className="text-center mb-5" sx={{ textShadow: "12px 12px 12px rgb(150,150,150)" }}>
+                404 <br />
+                Page Non Trouvée 😢
+            </Typography>
+            <Link href="/">
+                <Slide direction="up" in={true} timeout={{ enter: 1000 }} mountOnEnter unmountOnExit>
+                    <Button
+                        size="large"
+                        sx={{
+                            "&:hover": {
+                                background: "none"
+                            }
+                        }}
+                        startIcon={<ArrowBackIcon />}
+                    >
+                        Retour à l'accueil
+                    </Button>
+                </Slide>
+            </Link>
+        </div>
+    );
 }
