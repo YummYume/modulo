c056bde0dbf7323d5d76405393ffc4fe8b1e041e
Tristan-Evenot <evenot.tristan@gmail.com>
2022-05-09 20:48:38 +0200

    page 500 style and legal notice

--- app/pages/500.js
+++ app/pages/500.js
@@ -1,3 +1,31 @@
+import Typography from "@mui/material/Typography";
+import Slide from "@mui/material/Slide";
+import Button from "@mui/material/Button";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import Link from "next/link";
+
 export default function Custom500() {
-    return <h1>500 - Server-side error occurred</h1>;
+    return (
+        <div className="d-flex flex-column justify-content-center align-items-center w-100">
+            <Typography variant="h1" className="text-center mb-5" sx={{ textShadow: "12px 12px 12px rgb(150,150,150)" }}>
+                500 <br />
+                Server-side error occurred 😢
+            </Typography>
+            <Link href="/">
+                <Slide direction="up" in={true} timeout={{ enter: 1000 }} mountOnEnter unmountOnExit>
+                    <Button
+                        size="large"
+                        sx={{
+                            "&:hover": {
+                                background: "none"
+                            }
+                        }}
+                        startIcon={<ArrowBackIcon />}
+                    >
+                        Retour à l'accueil
+                    </Button>
+                </Slide>
+            </Link>
+        </div>
+    );
 }
