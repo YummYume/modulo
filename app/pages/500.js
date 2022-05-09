e6145ee7f04206fecf6a8b9ff8a36abdd5a6bbeb
YamYam <givsoul@gmail.com>
2022-05-09 22:10:08 +0200

    Fix 500 page

--- app/pages/500.js
+++ app/pages/500.js
@@ -9,23 +9,25 @@ export default function Custom500() {
         <div className="d-flex flex-column justify-content-center align-items-center w-100">
             <Typography variant="h1" className="text-center mb-5" sx={{ textShadow: "12px 12px 12px rgb(150,150,150)" }}>
                 500 <br />
-                Server-side error occurred 😢
+                Une erreur est survenue 😢
             </Typography>
-            <Link href="/">
-                <Slide direction="up" in={true} timeout={{ enter: 1000 }} mountOnEnter unmountOnExit>
-                    <Button
-                        size="large"
-                        sx={{
-                            "&:hover": {
-                                background: "none"
-                            }
-                        }}
-                        startIcon={<ArrowBackIcon />}
-                    >
-                        Retour à l'accueil
-                    </Button>
-                </Slide>
-            </Link>
+            <Slide direction="up" in={true} timeout={{ enter: 1000 }} mountOnEnter unmountOnExit>
+                <Link href="/">
+                    <a>
+                        <Button
+                            size="large"
+                            sx={{
+                                "&:hover": {
+                                    background: "none"
+                                }
+                            }}
+                            startIcon={<ArrowBackIcon />}
+                        >
+                            {"Retour à l'accueil"}
+                        </Button>
+                    </a>
+                </Link>
+            </Slide>
         </div>
     );
 }
