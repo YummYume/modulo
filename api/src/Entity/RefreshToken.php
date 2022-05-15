822c23aa2d47e6020ce9a21de79f464b568c82d2
YamYam <givsoul@gmail.com>
2022-05-15 21:31:07 +0200

    Updated refresh_tokens table

--- api/src/Entity/RefreshToken.php
+++ api/src/Entity/RefreshToken.php
@@ -4,11 +4,34 @@ namespace App\Entity;
 
 use App\Entity\Traits\TimestampableTrait;
 use Doctrine\ORM\Mapping as ORM;
-use Gesdinet\JWTRefreshTokenBundle\Entity\RefreshToken as BaseRefreshToken;
+use Gesdinet\JWTRefreshTokenBundle\Entity\RefreshTokenRepository;
+use Gesdinet\JWTRefreshTokenBundle\Model\AbstractRefreshToken;
 
-#[ORM\Entity()]
+#[ORM\Entity(repositoryClass: RefreshTokenRepository::class)]
 #[ORM\Table(name: '`refresh_tokens`')]
-class RefreshToken extends BaseRefreshToken
+class RefreshToken extends AbstractRefreshToken
 {
     use TimestampableTrait;
+
+    #[ORM\Id]
+    #[ORM\Column(name: 'id', type: 'integer', nullable: false)]
+    #[ORM\GeneratedValue(strategy: 'AUTO')]
+    protected $id;
+
+    #[ORM\Column(name: 'refresh_token', type: 'string', nullable: false)]
+    protected $refreshToken;
+
+    #[ORM\Column(name: 'uuid', type: 'string', nullable: false)]
+    protected $username;
+
+    #[ORM\Column(name: 'valid', type: 'datetime', nullable: false)]
+    protected $valid;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 }
