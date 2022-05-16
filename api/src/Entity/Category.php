22fc57762ebc05ebc986fa472ba64316aaaeadc8
Tristan-Evenot <evenot.tristan@gmail.com>
2022-05-16 10:02:05 +0200

    category invited roles

--- api/src/Entity/Category.php
+++ api/src/Entity/Category.php
@@ -9,6 +9,7 @@ use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
 #[ApiResource]
@@ -32,6 +33,7 @@ class Category
     private Collection $roles;
 
     #[ORM\ManyToMany(targetEntity: Role::class, inversedBy: 'defaultCategories')]
+    #[Assert\Expression(expression: "value in this.getRoles().toArray()", message: "category.invited_roles.expression")]
     private Collection $invitedRoles;
 
     public function __construct()
