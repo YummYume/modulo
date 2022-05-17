e69745a380a59b87d5803d5b4c4d900cbb6f2159
LeoBruant <bruantleo@gmail.com>
2022-05-17 16:58:00 +0200

    Add event in progress

--- app/api/event.js
+++ app/api/event.js
@@ -1,5 +1,6 @@
 import { axiosApiInstance } from "./axios/api";
 
+<<<<<<< HEAD
 export const addEvent = async (name, description, active, startDate, endDate, scope, categories, participants) => {
     const response = await axiosApiInstance().post("/events", {
         name,
@@ -14,3 +15,10 @@ export const addEvent = async (name, description, active, startDate, endDate, sc
 
     return response;
 };
+=======
+const add = async (data) => {
+    const response = await axiosApiInstance().post("/events", data);
+
+    return response;
+}
+>>>>>>> Add event in progress
