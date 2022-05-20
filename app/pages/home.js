64b5850a0c4d36e89763c387fcda4b5b7a36981b
LeoBruant <bruantleo@gmail.com>
2022-05-20 22:42:10 +0200

    Added DateTime pickers

--- app/pages/home.js
+++ app/pages/home.js
@@ -10,13 +10,16 @@ import TextField from "@mui/material/TextField";
 import Switch from "@mui/material/Switch";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormGroup from "@mui/material/FormGroup";
-import IconButton from "@mui/material/IconButton";
-import AddCircleIcon from "@mui/icons-material/AddCircle";
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 import LoadingButton from "@mui/lab/LoadingButton";
 import CheckIcon from "@mui/icons-material/Check";
 import { toast } from "react-toastify";
+import DateTimePicker from "@mui/lab/DateTimePicker";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import AdapterMoment from "@mui/lab/AdapterMoment";
 
 import { getCurrentUserFromServer } from "../api/user";
 import { isGranted, features } from "../services/user";
@@ -73,9 +76,113 @@ export default function Home() {
                 <Typography variant="h2" component="h1" className="text-center text-break my-5">
                     Accueil Connecté
                 </Typography>
-                {isGranted(features.AGENDA_ACCESS, user) && (
-                    <Calendar localizer={localizer} events={events} startAccessor="start" endAccessor="end" style={{ height: 500 }} />
-                )}
+                <Calendar localizer={localizer} events={events} startAccessor="start" endAccessor="end" style={{ height: 500 }} />
+                <Fab color="primary" className="mx-auto d-block mt-5" onClick={() => setOpenModal(true)}>
+                    <AddIcon />
+                </Fab>
+                <Modal open={openModal} onClose={() => setOpenModal(false)} className="d-flex justify-content-center align-items-center">
+                    <Box backgroundColor="box.index.backgroundLogin" maxWidth="90%" width="35rem" className="p-4 rounded">
+                        <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+                            {({ isSubmitting, values, touched, errors, handleChange, handleBlur }) => (
+                                <Form>
+                                    <Typography variant="h4" className="text-center my-4">
+                                        Créer un événement
+                                    </Typography>
+                                    <TextField
+                                        id="name"
+                                        name="name"
+                                        label="Nom"
+                                        variant="outlined"
+                                        fullWidth
+                                        value={values.name}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        error={!!errors.name && touched.name}
+                                        className="my-2"
+                                    />
+                                    <TextField
+                                        id="description"
+                                        name="description"
+                                        label="Description"
+                                        variant="outlined"
+                                        fullWidth
+                                        value={values.description}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        error={!!errors.description && touched.description}
+                                        className="my-2"
+                                    />
+                                    <LocalizationProvider dateAdapter={AdapterMoment}>
+                                        <DateTimePicker
+                                            value={values.startDate}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            error={!!errors.startDate && touched.startDate}
+                                            renderInput={(props) => (
+                                                <TextField
+                                                    {...props}
+                                                    id="startDate"
+                                                    name="startDate"
+                                                    label="Date de début"
+                                                    variant="outlined"
+                                                    fullWidth
+                                                    className="my-2"
+                                                />
+                                            )}
+                                        />
+                                    </LocalizationProvider>
+                                    <LocalizationProvider dateAdapter={AdapterMoment}>
+                                        <DateTimePicker
+                                            value={values.endDate}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            error={!!errors.endDate && touched.endDate}
+                                            renderInput={(props) => (
+                                                <TextField
+                                                    {...props}
+                                                    id="endDate"
+                                                    name="endDate"
+                                                    label="Date de fin"
+                                                    variant="outlined"
+                                                    fullWidth
+                                                    className="my-2"
+                                                />
+                                            )}
+                                        />
+                                    </LocalizationProvider>
+                                    <FormGroup>
+                                        <FormControlLabel
+                                            control={
+                                                <Switch
+                                                    id="active"
+                                                    name="active"
+                                                    variant="outlined"
+                                                    fullWidth
+                                                    value={values.active}
+                                                    onChange={handleChange}
+                                                    onBlur={handleBlur}
+                                                    error={!!errors.active && touched.active}
+                                                />
+                                            }
+                                            label="Actif"
+                                        />
+                                    </FormGroup>
+                                    <div className="text-center">
+                                        <LoadingButton
+                                            loading={isSubmitting || addEventMutation.isLoading}
+                                            type="submit"
+                                            variant="contained"
+                                            loadingPosition="end"
+                                            endIcon={<CheckIcon />}
+                                        >
+                                            Valider
+                                        </LoadingButton>
+                                    </div>
+                                </Form>
+                            )}
+                        </Formik>
+                    </Box>
+                </Modal>
             </div>
         </React.Fragment>
     );
