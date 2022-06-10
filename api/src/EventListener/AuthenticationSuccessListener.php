90e5156ea6459f9e5308bcc09a2d523068c54022
YamYam <givsoul@gmail.com>
2022-06-10 22:59:11 +0200

    Hopefully fixed prod

--- api/src/EventListener/AuthenticationSuccessListener.php
+++ api/src/EventListener/AuthenticationSuccessListener.php
@@ -8,11 +8,16 @@ use App\Service\User\UserManager;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 final class AuthenticationSuccessListener
 {
-    public function __construct(private UserNormalizer $userNormalizer, private UserManager $userManager, private RequestStack $requestStack)
-    {
+    public function __construct(
+        private UserNormalizer $userNormalizer,
+        private UserManager $userManager,
+        private RequestStack $requestStack,
+        private NormalizerInterface $normalizer
+    ) {
     }
 
     public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event): void
@@ -26,7 +31,7 @@ final class AuthenticationSuccessListener
 
         $this->userManager->setCurrentScopeFromId($user, $request->get('scope'));
 
-        $data = $this->userNormalizer->normalize($user, 'jsonld', ['groups' => ['get:me']]);
+        $data = $this->normalizer->normalize($user, 'jsonld', ['groups' => ['get:me']]);
 
         $event->setData($data);
     }
